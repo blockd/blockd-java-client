1d7206a42e5eb5ece9b0f8686fe41e06b5eb6ba2
Brian Manley <brian.manley@gmail.com>
2013-10-15 19:44:05 -0700

    Updated for changes in clients

--- src/test/java/com/frs/blockd/BlockdClientTest.java
+++ src/test/java/com/frs/blockd/BlockdClientTest.java
@@ -8,18 +8,18 @@ public class BlockdClientTest {
     @Test
     public void rockyTest() throws Exception {
 
-        BlockdClient client = new BlockdClient("localhost", 11311);
+        SimpleClient client = new SimpleClient("localhost", 11311);
         client.connect();
         System.out.println(client.wisdom());
-        System.out.println(client.aquire("APOLLO"));
-        System.out.println(client.aquire("MICK", 1000));
-        System.out.println(client.aquire("ROCKY", 2000, 'R'));
+        System.out.println(client.acquire("APOLLO"));
+        System.out.println(client.acquire("MICK", 1000));
+        System.out.println(client.acquire("ROCKY", 2000, 'R'));
         Thread.sleep(1000);
         System.out.println(client.show());
         System.out.println(client.release("ROCKY"));
         System.out.println(client.release("APOLLO"));
         System.out.println(client.release("MICK"));
-        client.close();
+        client.quit();
     }
 
 }
